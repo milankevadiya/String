7a1ed60ed582e5855c751354dffff4ef626a0b5e
milankevadiya <milan.kvd1991@gmail.com>
2021-10-19 14:32:05 +0100

    addition added

--- src/StringPractise.java
+++ src/StringPractise.java
@@ -1,11 +1,13 @@
 public class StringPractise {
 
-
+static int a=10;
+static int b = 5;
     public static void main(String[] args){
 
         System.out.println("this is string");
         System.out.println("test");
-        //System.out.println("this is string");
+        System.out.println("this is string");
+        System.out.println(a+b);
     }
 
 }
